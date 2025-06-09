78ad0ecfc7df33f58ca3d8526552a232e09cd11d
Carlos11232 <carloscerezo212@gmail.com>
2025-06-09 10:06:32 +0200

    Update ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,1 +1,12 @@
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  } else {
+    console.log(numberTwo);
+  }
+}
+
+// Ejemplo de uso:
+greaterNumber(8, 15); // Mostrará: 15
+greaterNumber(20, 10); // Mostrará: 20
 
