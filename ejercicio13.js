b23bd426b1afa567ec91dde72c23b4fa26c3a056
Carlos11232 <carloscerezo212@gmail.com>
2025-06-09 10:11:15 +0200

    Update ejercicio13.js

--- ejercicio13.js
+++ ejercicio13.js
@@ -1,1 +1,30 @@
+function nameFinder(nameList, nameToFind) {
+  const index = nameList.indexOf(nameToFind);
+  if (index !== -1) {
+    // Si existe, devuelve true y la posición
+    return { found: true, position: index };
+  } else {
+    // Si no existe, devuelve false
+    return false;
+  }
+}
+
+// Prueba con el array de ejemplo
+const names = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+
+// Ejemplos de uso:
+console.log(nameFinder(names, 'Tony'));    // { found: true, position: 2 }
+console.log(nameFinder(names, 'Pepper'));  // false
 
