18332d7fc2d6c87e3910997be3cff14f030137c1
Carlos11232 <carloscerezo212@gmail.com>
2025-06-09 10:10:46 +0200

    Update ejercicio12.js

--- ejercicio12.js
+++ ejercicio12.js
@@ -1,1 +1,23 @@
+function removeDuplicates(list) {
+  // Usamos un Set para eliminar duplicados y luego lo convertimos de nuevo a array
+  return [...new Set(list)];
+}
+
+// Prueba con el array de ejemplo
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+
+console.log(removeDuplicates(duplicates));
+// Mostrará: ['sushi', 'pizza', 'burger', 'potatoe', 'pasta', 'ice-cream', 'chicken', 'onion rings', 'soda']
 
