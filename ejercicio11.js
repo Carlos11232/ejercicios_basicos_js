a12367f58b5678283471a9aa29cb10c1df1acc8b
Carlos11232 <carloscerezo212@gmail.com>
2025-06-09 10:08:44 +0200

    Update ejercicio11.js

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,1 +1,16 @@
+function averageWord(list) {
+  let suma = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (typeof list[i] === 'number') {
+      suma += list[i];
+    } else if (typeof list[i] === 'string') {
+      suma += list[i].length;
+    }
+  }
+  return suma;
+}
+
+// Prueba con el array de ejemplo
+const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
+console.log(averageWord(mixedElements)); // Mostrará: 50
 
