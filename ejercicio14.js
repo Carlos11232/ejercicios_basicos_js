22e774bb5274bc32cbbf0a7da7117d8902052a30
Carlos11232 <carloscerezo212@gmail.com>
2025-06-09 10:11:51 +0200

    Update ejercicio14.js

--- ejercicio14.js
+++ ejercicio14.js
@@ -1,1 +1,30 @@
+function repeatCounter(list) {
+  const counts = {};
+  for (let word of list) {
+    if (counts[word]) {
+      counts[word]++;
+    } else {
+      counts[word] = 1;
+    }
+  }
+  return counts;
+}
+
+// Prueba con el array de ejemplo
+const words = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code'
+];
+
+console.log(repeatCounter(words));
+// Mostrará: { code: 4, repeat: 1, eat: 1, sleep: 3, enjoy: 2 }
 
